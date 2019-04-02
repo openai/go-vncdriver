e340686cd80d194fb42c2d20962a26e534af06ac
Max Lapan <max.lapan@gmail.com>
2019-04-02 15:18:52 +0300

    Fix for race condition with buffer update

--- gymvnc/gymvnc.go
+++ gymvnc/gymvnc.go
@@ -346,8 +346,9 @@ func (c *VNCSession) applyRect(conn *vncclient.ClientConn, rect vncclient.Rectan
 		screenEnd := screenStart + uint32(rect.Width)
 
 		bytes += encEnd - encStart
-
-		copy(c.backScreen.Data[screenStart:screenEnd], colors[encStart:encEnd])
+		if c.backScreen != nil {
+			copy(c.backScreen.Data[screenStart:screenEnd], colors[encStart:encEnd])
+		}
 		// wg.Done()
 		// }(y)
 	}
